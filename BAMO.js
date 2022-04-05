b1083e64918a148f57701bd118a3529743097bda
mudwayt <mudwayt@mcmaster.ca>
2022-04-05 19:13:19 -0400

    Revamped blockbench plugin interface

--- BAMO.js
+++ BAMO.js
@@ -153,74 +153,170 @@
             }
         },
         template: `
-        <div class="container pt-5 pb-5 center">
+        <div class="wizardWrapper">
 
-            <div v-if="step=='start'">
-                <h3>BAMO Editor</h3><br>
-                <div>
-                    <p v-if="error==true" style="color:red">Block display name cannot be blank</p>
-                    <input type="text" class="dark_bordered form-control form-control-lg" v-model="form.displayName" placeholder="Block Display Name"><br><br>
-                    <button @click.prevent="createJSON">Instant</button>
-                    <button @click.prevent="startAdvanced">Advanced</button>
-                </div>
+            <ul id="sideBar">
+                <li v-bind:class="{selected: step =='start'}" @click.prevent="changePage($event, 'start')">
+                    Start
+                </li>
+                <li class="sideBarHeader">Properties</li>
+                <li v-bind:class="{selected: step == 'physical'}" @click.prevent="changePage($event, 'physical')">
+                    Physical
+                </li>
+                <li v-bind:class="{selected: step == 'advPhys'}" @click.prevent="changePage($event, 'advPhys')">
+                    Adv Physical
+                </li>
+                <li v-bind:class="{selected: step == 'other'}" @click.prevent="changePage($event, 'other')">
+                    Other
+                </li>
+                <li class="sideBarHeader">Adv. Options</li>
+                <li v-bind:class="{selected: step == 'settings'}" @click.prevent="changePage($event, 'settings')">
+                    BAMO settings
+                </li>
+            </ul>
+
+            <!-- Start -->
+            <div class="wizardPage" v-if="step=='start'">
+                <h1 style="padding:0; text-align: center;">BAMO Exporter</h1>
+                <p align=center>Click "Export" at any time to export the model. Any unmodified properties will be set to the default values for a dirt block. Changes will persist until Blockbench is closed.</p>
+
+                <ul class="settingsList">
+                    <li>
+                        <div class="headerLabel">Display Name</div>
+                        <p class="headerDescription">The unique name of the block to be exported</p>
+                        <p v-if="error==true" style="color:red">Block display name cannot be blank</p>
+                        <input type="text" class="dark_bordered" style="" v-model="form.displayName" placeholder="blockname">
+                    </li>
+                </ul><br>
+                
+                <button @click.prevent="changePage($event, 'physical')">Continue</button>
+                <button @click.prevent="createJSON" style = "float: right;">Export</button>
             </div>
 
-            <div v-if="step=='advanced'">
-                <h3>Advanced Block Creation</h3>
-                <div>
-                    <table>
-                        <tr>
-                            <td>Material: </td>
-                            <td><select v-model="form.material"><option v-for="op in materialOptions" v-bind:value="op">{{ op }}</option></select></td>
-                        </tr>
-                        <tr>
-                            <td>Blast Resistance: </td>
-                            <td><input type="number" class="dark_bordered" v-model.number="form.blastRes" min=0></td>
-                        </tr>
-                        <tr>
-                            <td>Slipperiness: </td>
-                            <td><input type="number" class="dark_bordered" v-model.number="form.slip" min=0></td>
-                        </tr>
-                        <tr>
-                            <td>Gravity: </td>
-                            <td><input type="checkbox" v-model="form.gravity"></td>
-                        </tr>
-                        <tr>
-                            <td>Rotation Type: </td>
-                            <td><select id="RotationType" v-model="form.rotType"><option v-for="op in rotationTypes" v-bind:value="op">{{ op }}</option></select></td>
-                        </tr>
-                        <tr>
-                            <td>Block Sounds: </td>
-                            <td><select v-model="form.sounds"><option v-for="op in soundOptions" v-bind:value="op">{{ op }}</option></select></td>
-                        </tr>
-                        <tr>
-                            <td>Luminance: </td>
-                            <td><input type="number" class="dark_bordered" v-model.number="form.lum" min=0></td>
-                        </tr>
-                        <tr>
-                            <td>Max Stack Size: </td>
-                            <td><input type="number" class="dark_bordered" v-model.number="form.maxStack" min=0></td>
-                        </tr>
-                        <tr>
-                            <td>Fireproof: </td>
-                            <td><input type="checkbox" v-model="form.fireproof"></td>
-                        </tr>
-                        <tr>
-                            <td>Transparency: </td>
-                            <td><select v-model="form.transparency"><option v-for="tr in transparencyOptions" v-bind:value="tr">{{ tr }}</option></select></td>
-                        </tr>
-                        <tr>
-                            <td>Creative Tab: </td>
-                            <td><select v-model="form.creativeTab"><option v-for="op in tabOptions" v-bind:value="op">{{ op }}</option></select></td>
-                        </tr>
-                    </table>
-                    <br>
-                    <button @click.prevent="createJSON">Export</button>
-                </div>
+            <!-- Physical Properties -->
+            <div class="wizardPage" v-if="step=='physical'">
+                <h1 style="padding:0; text-align: center;">Physical Properties</h1>
+                <ul class="settingsList">
+                    <li>
+                        <div class = "headerLabel">Material</div>
+                        <p class="headerDescription">Sets default properties to match those of the listed block. For a list of properties each block has, see the <a href="https://minecraft.fandom.com/wiki/Materials">Minecraft wiki</a></p>
+                        <select class="dark_bordered" v-model="form.material"><option v-for="op in materialOptions" v-bind:value="op">{{ op }}</option></select>
+                    </li>
+
+                    <li>
+                        <div class = "headerLabel">Block Sounds</div>
+                        <p class="headerDescription">Sets the sounds made when the custom block is stepped on/broken/etc to match the listed block</p>
+                        <select class="dark_bordered" v-model="form.sounds"><option v-for="op in soundOptions" v-bind:value="op">{{ op }}</option></select>
+                    </li>
+
+                    <li>
+                        <div class="headerLabel">Transparency</div>
+                        <p class="headerDescription">The transparency style of the block. Models with partially transparent textures should be set to Cutout</p>
+                        <select class="dark_bordered" v-model="form.transparency"><option v-for="tr in transparencyOptions" v-bind:value="tr">{{ tr }}</option></select>
+                    </li>
+                    
+                    <li>
+                        <input type="checkbox" v-model="form.gravity">
+                        <div class = "headerLabel">Gravity</div>
+                        <p class="headerDescription">Determines if the block falls due to gravity</p>
+                    </li>
+                </ul>
+                <button @click.prevent="changePage($event, 'start')">Previous</button>
+                <button @click.prevent="changePage($event, 'advPhys')">Continue</button>
+                <button @click.prevent="createJSON" style = "float: right;">Export</button>
+            </div>
+            
+            <!-- Adv Physical Properties -->
+            <div class="wizardPage" v-if="step=='advPhys'">
+                <h1 style="padding:0; text-align: center;">Advanced Physical Properties</h1>
+                <ul class="settingsList">
+                    <li>
+                        <input type="number" class="dark_bordered" v-model.number="form.lum" min=0 max=15>
+                        <div class = "headerLabel">Luminance</div>
+                        <p class="headerDescription">The light level of the block. Ranges from 0 to 15. Torches default at 14</p>
+                    </li>
+
+                    <li>
+                        <input type="number" class="dark_bordered" v-model.number="form.blastRes" min=0>
+                        <div class = "headerLabel">Blast Resistance</div>
+                        <p class="headerDescription">How resistant the block is to explosions. Most blocks fall between 0.1 and 9</p>
+                    </li>
+
+                    <li>
+                        <input type="number" class="dark_bordered" v-model.number="form.slip" min=0 step="0.1">
+                        <div class = "headerLabel">Slipperiness</div>
+                        <p class="headerDescription">How much you slide when crossing the block. Default is 0.6 and reaches 0.989 for blue ice</p>
+                    </li>
+
+                    <li>
+                        <input type="checkbox" v-model="form.fireproof">
+                        <div class = "headerLabel">Fireproof</div>
+                        <p class="headerDescription">Determines if the block will burn</p>
+                    </li>
+                </ul>
+                <button @click.prevent="changePage($event, 'physical')">Previous</button>
+                <button @click.prevent="changePage($event, 'other')">Continue</button>
+                <button @click.prevent="createJSON" style = "float: right;">Export</button>
+            </div>
+
+            <!-- Other Properties -->
+            <div class="wizardPage" v-if="step=='other'">
+                <h1 style="padding:0; text-align: center;">Other Properties</h1>
+                <ul class="settingsList">
+                   
+                    <li>
+                        <input type="number" class="dark_bordered" v-model.number="form.maxStack" min=0>
+                        <div class = "headerLabel">Max Stack Size</div>
+                        <p class="headerDescription">The maximum number the block in a stack</p>
+                    </li>
+
+                    <li>
+                        <div class = "headerLabel">Creative Tab</div>
+                        <p class="headerDescription">Which tab on the creative inventory menu the block will be added to</p>
+                        <select class="dark_bordered" v-model="form.creativeTab"><option v-for="op in tabOptions" v-bind:value="op">{{ op }}</option></select>
+                    </li>
+
+                    <li>
+                        <div class = "headerLabel">Rotation Type</div>
+                        <p class="headerDescription">WIP: Currently not working</p>
+                        <select class="dark_bordered" v-model="form.rotType"><option v-for="op in rotationTypes" v-bind:value="op">{{ op }}</option></select>
+                    </li>
+
+                    <li>
+                        <div class = "headerLabel">Block Type</div>
+                        <p class="headerDescription">What type of block (regular/stair/slab/etc) the model is (WIP)</p>
+                    </li>
+                <br>
+                <button @click.prevent="changePage($event, 'advPhys')">Previous</button>
+                <button @click.prevent="createJSON" style = "float: right;">Export</button>
+            </div>
+
+            <div class="wizardPage" v-if="step=='settings'">
+                <h1 style="padding:0; text-align: center;">BAMO Export Settings</h1>
+                <ul class="settingsList">
+                    <li>
+                        <div class = "headerLabel">Only export textures currently used</div>
+                        <p class="headerDescription">WIP</p>
+                    </li>
+
+                    <li>
+                        <div class = "headerLabel">reuse model file</div>
+                        <p class="headerDescription">WIP</p>
+                    </li>
+                </ul>
             </div>
         </div>
         `,
         methods: {
+
+            changePage: function(event, page){
+                if (this.form.displayName == ""){
+                    this.error = true;
+                }else{
+                    this.error = false;
+                    this.step = page;
+                }
+            },
 
             reset: function(event){
                 this.error = false;
@@ -277,7 +373,8 @@
                     }else{
                         // KINDA CRUDE, NEEDS UPDATING
                         splitTexture = modelData.textures[key].split(":");
-                        modelData.textures[key] = this.form.namespace + ":blocks/" + splitTexture[1];
+                        imageName = splitTexture[1].split("/")
+                        modelData.textures[key] = this.form.namespace + ":blocks/" + imageName.pop();
                     }
                 })
 
@@ -313,18 +410,11 @@
                 let e = open_interface;
                 e.hide();
             },
-
-            startAdvanced:function(event){
-                if (this.form.displayName == ""){
-                    this.error = true;
-                }else{
-                    this.error = false;
-                    this.step = 'advanced';
-                }
-            }
         }
     }
     
+    var cssData;
+
     Plugin.register('BAMO', {
         title: 'BAMO Exporter',
         author: 'Ryytikki',
@@ -355,7 +445,7 @@
                     if (Project.name != undefined){
                         if ((Settings.get('minecraftFolder') != undefined) && (Settings.get('minecraftFolder') != '')){
                             exportWindow.show();
-                            exportWindow.content_vue.reset();
+                            //exportWindow.content_vue.reset();
                         }else{
                             Blockbench.showMessageBox({buttons: ["Ok"], title: "Error", message: "You must set your Resources folder location under Settings->Export"});
                         }
@@ -370,10 +460,99 @@
             // Dialog that opens when you click the button1
             // See the vueComponent object for details
             exportWindow = new Dialog('C&CExportWindow', {
+                id: "BAMO",
                 title: 'BAMO Exporter',
                 component: vueComponent,
                 buttons: [],
+                padding: !1,
+                width: 720,
+                height: 620,
             });
+
+            cssData = Blockbench.addCSS(`
+                #BAMO .dialog_content {
+                    margin: 0;
+                } 
+
+                #BAMO .wizardWrapper {
+                    display: flex; 
+                    flex-direction: row;
+                    height: 100%;
+                } 
+
+                #BAMO .wizardPage {
+                    width: 100%; 
+                    padding: 12px 20px;
+                } 
+
+                #BAMO .headerLabel {
+                    font-size: 20px; 
+                    display: inline-block
+                } 
+
+                #BAMO .headerDescription {
+                    color: white;
+                } 
+                
+                #BAMO input[type='text'] {
+                    display: block; 
+                    width: 100%; 
+                    height: auto; 
+                    padding: 4px 8px; 
+                    border: 1px solid black;
+                } 
+                
+                #BAMO input[type='number']{
+                    display: inline-block; 
+                    width: 50px; 
+                    height: auto; 
+                    border: 1px solid black; 
+                    padding-left: 4px; 
+                    margin-right: 8px; 
+                    text-align: right;
+                } 
+                
+                #BAMO input[type='checkbox'] {
+                    padding-right: 8px;
+                } 
+                
+                #BAMO select {
+                    width: 100%; 
+                    height: auto; 
+                    padding: 4px 8px;
+                    border: 1px solid black; 
+                } 
+                
+                #sideBar {
+                    border-right: 2px solid black;
+                    width: 175px;
+                } 
+                
+                #sideBar li {
+                    width: 100%; 
+                    padding: 8px 20px; 
+                    cursor: pointer;
+                } 
+                
+                #sideBar li.selected {
+                    width: 100%; 
+                    padding: 8px 20px; 
+                    border-right: 4px solid green; 
+                    cursor: default;
+                    background-color: gray;
+                } 
+
+                #sideBar li.sideBarHeader {
+                    width: 100%;
+                    padding: 2px 10px;
+                    font-size: 12px;
+                    cursor: default;
+                }
+                
+                ul.settingsList li {
+                    padding: 8px 0;
+                }
+            `);
 
             // Pull the filename when loading/saving to use for file dialog
             Blockbench.on("load_project", function() {
@@ -393,6 +572,7 @@
         onunload() {
             btn.delete();
             minecraftFolder.delete();
+            cssData.delete();
         }
     });
 
